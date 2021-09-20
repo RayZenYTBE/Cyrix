882f4bb3d798ae3e0170c69ece4776cadbdfd7fc
AtMostFearDevs <87268716+RayZenYTBE@users.noreply.github.com>
2021-09-20 18:35:30 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
-    console.log(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
+    console.log(`Successfully logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users! Thanks for using Cyrix Bot by AtMostFear!`);
 
     client.user.setActivity(client.config.discord.activity);
 };
